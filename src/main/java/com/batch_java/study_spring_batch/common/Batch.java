ce8c3c58d23261e73a6c03feb54f743d0a33fba5
djccnt15 <djccnt15@gmail.com>
2023-11-18 12:49:02 +0900

    create basic settings for Spring Batch

---
+++ src/main/java/com/batch_java/study_spring_batch/common/Batch.java
@@ -0,0 +1,18 @@
+package com.batch_java.study_spring_batch.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.AliasFor;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Configuration
+public @interface Batch {
+    
+    @AliasFor(annotation = Configuration.class)
+    String value() default "";
+}
