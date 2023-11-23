ac88a4a452a8418e4c4ea601a6bdc5140f56cea6
djccnt15 <djccnt15@gmail.com>
2023-11-24 00:41:14 +0900

    create sample for File ItemReader

---
+++ src/main/java/com/batch_java/study_spring_batch/batch/ItemReaderBatchConfiguration.java
@@ -0,0 +1,71 @@
+package com.batch_java.study_spring_batch.batch;
+
+import com.batch_java.study_spring_batch.common.Batch;
+import com.batch_java.study_spring_batch.model.User;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.json.JacksonJsonObjectReader;
+import org.springframework.batch.item.json.JsonItemReader;
+import org.springframework.batch.item.json.builder.JsonItemReaderBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Slf4j
+@Batch
+public class ItemReaderBatchConfiguration {
+
+    @Bean
+    public Job job(
+        JobRepository jobRepository,
+        Step step
+    ) {
+        return new JobBuilder("itemReaderJob", jobRepository)
+            .incrementer(new RunIdIncrementer())
+            .start(step)
+            .build();
+    }
+    
+    @Bean
+    public Step step(
+        JobRepository jobRepository,
+        PlatformTransactionManager transactionManager,
+        ItemReader<User> jsonItemReader
+    ) {
+        return new StepBuilder("step", jobRepository)
+            .<User, User>chunk(2, transactionManager)
+            .reader(jsonItemReader)
+            .writer(System.out::println)
+            .build();
+    }
+    
+    @Bean
+    public FlatFileItemReader<User> flatFileItemReader() {
+        return new FlatFileItemReaderBuilder<User>()
+            .name("flatFileItemReader")
+            .resource(new ClassPathResource("users.txt"))
+            .linesToSkip(1)
+            .delimited().delimiter(",")
+            .names("name", "age", "region", "phoneNumber")
+            .targetType(User.class)
+            .strict(true)
+            .build();
+    }
+    
+    @Bean
+    public JsonItemReader<User> jsonItemReader() {
+        return new JsonItemReaderBuilder<User>()
+            .name("jsonItemReader")
+            .resource(new ClassPathResource("users.json"))
+            .jsonObjectReader(new JacksonJsonObjectReader<>(User.class))
+            .build();
+    }
+}
---
+++ src/main/java/com/batch_java/study_spring_batch/model/User.java
@@ -0,0 +1,12 @@
+package com.batch_java.study_spring_batch.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+    private String name;
+    private Integer age;
+    private String region;
+    private String phoneNumber;
+}
