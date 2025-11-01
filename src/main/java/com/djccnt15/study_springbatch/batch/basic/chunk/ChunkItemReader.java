b36538d55788cf766553c84132ddc579f671f28a
Jay.D <djccnt15@gmail.com>
2025-11-01 20:55:20 +0900

    update `ChunkItemReader.java` add comment about `BufferedReader`

--- src/main/java/com/djccnt15/study_springbatch/batch/basic/chunk/ChunkItemReader.java
+++ src/main/java/com/djccnt15/study_springbatch/batch/basic/chunk/ChunkItemReader.java
@@ -4,6 +4,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.item.ItemReader;
 
 // 데이터 소스에서 데이터를 하나씩 반환. `read()` 메서드가 호출될 때마다 데이터를 순차적으로 반환
+// 내부적으로 `BufferedReader`를 사용해 일정 크기(16kb)의 데이터를 버퍼에 저장 후 순차적 출력
 // 청크 크기 만큼 `read()` 메서드를 호출하여 하나의 청크 생성
 @Slf4j
 public class ChunkItemReader implements ItemReader<Integer> {
