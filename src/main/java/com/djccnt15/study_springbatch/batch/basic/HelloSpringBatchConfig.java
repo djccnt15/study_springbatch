6bc872de6d2bef962040aa9b77ad36c0facefde3
Jay.D <djccnt15@gmail.com>
2025-11-09 21:36:47 +0900

    update `HelloSpringBatchConfig.java` add jobParameters `helloId` for restart

--- src/main/java/com/djccnt15/study_springbatch/batch/basic/HelloSpringBatchConfig.java
+++ src/main/java/com/djccnt15/study_springbatch/batch/basic/HelloSpringBatchConfig.java
@@ -1,42 +1,47 @@
 package com.djccnt15.study_springbatch.batch.basic;
 
 import com.djccnt15.study_springbatch.annotation.Batch;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Slf4j
 @Batch
+@RequiredArgsConstructor
 public class HelloSpringBatchConfig {
     
+    private final JobRepository jobRepository;
+    private final PlatformTransactionManager transactionManager;
+    
     @Bean
-    public Job helloWorldJob(
-        JobRepository jobRepository,
-        Step helloWorldStep
-    ){
+    public Job helloWorldJob(){
         return new JobBuilder("helloWorldJob", jobRepository)
-            .start(helloWorldStep)
+            .start(helloWorldStep(null))
             .build();
     }
     
     @Bean
-    public Step helloWorldStep(
-        JobRepository jobRepository,
-        PlatformTransactionManager transactionManager
-    ) {
+    public Step helloWorldStep(Tasklet helloWorldTasklet) {
         return new StepBuilder("helloWorldStep", jobRepository)
-            .tasklet(helloWorldTasklet(), transactionManager)
+            .tasklet(helloWorldTasklet, transactionManager)
             .build();
     }
     
-    public Tasklet helloWorldTasklet() {
+    @Bean
+    @StepScope
+    public Tasklet helloWorldTasklet(
+        @Value("#{jobParameters['helloId']}") String helloId
+    ) {
         return (contribution, chunkContext) -> {
             System.out.println("=== HELLO SPRING BATCH ===");
             return RepeatStatus.FINISHED;
