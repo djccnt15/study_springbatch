c47cf635c2c77e54ae12911d6f7e7881500a8d75
Jay.D <djccnt15@gmail.com>
2025-11-18 21:53:23 +0900

    update `StudySpringbatchApplication.java` for stdout exit code

--- src/main/java/com/djccnt15/study_springbatch/StudySpringbatchApplication.java
+++ src/main/java/com/djccnt15/study_springbatch/StudySpringbatchApplication.java
@@ -7,7 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class StudySpringbatchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StudySpringbatchApplication.class, args);
+		// SpringApplication.run(StudySpringbatchApplication.class, args);
+		// Job 실행 결과에 따른 종료 코드(BatchStatus enum의 ordinal)를 Spring 애플리케이션 종료 코드로 출력
+        System.exit(SpringApplication.exit(
+            SpringApplication.run(StudySpringbatchApplication.class, args)
+        ));
 	}
 
 }
