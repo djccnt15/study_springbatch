f2e58c9e7d8cdd71e1a7867c7cf986d1dcda51c9
Jay.D <djccnt15@gmail.com>
2025-11-25 23:05:37 +0900

    update `StepFlowBatchConfig.java` add comments about implicit ExitCode of `Flow`

--- src/main/java/com/djccnt15/study_springbatch/batch/flow/StepFlowBatchConfig.java
+++ src/main/java/com/djccnt15/study_springbatch/batch/flow/StepFlowBatchConfig.java
@@ -31,7 +31,11 @@ public class StepFlowBatchConfig {
     ) {
         return new JobBuilder("stepFlowJob", jobRepository)
             .start(stepFlowStep1)  // 시작
+            // 와일드카드(*, ?)를 이용한 패턴 매칭 규칙으로 여러 ExitCode에 대한 규칙을 지정할 수 있음
             .on("*").to(stepFlowStep2)  // 이전 단계의 수행 결과에 따라 다음 단계 싷행 여부 지정
+            // stepFlowStep2 와 같이 .on() -> .to()가 완성되지 않은 step들은 암시적 규칙 자동 생성
+            // ExitCode가 COMPLETED인 경우: COMPLETED 상태의 EndState로 전이
+            // ExitCode가 COMPLETED가 아닌 경우: FAILED 상태의 EndState로 전이
             .from(stepFlowStep1)  // 이전 단계의 수행 결과에 따른 작업 분기점을 설정
             .on(FAILED).to(stepFlowStep3)
             .from(stepFlowStep3)
